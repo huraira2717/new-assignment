d129fe62c9ab776fd4e5e530f0ecfb78e57afa67
Huraira2717 <122014990+Huraira2717@users.noreply.github.com>
2023-02-26 23:05:39 +0500

    done

---
+++ app.js
@@ -0,0 +1,635 @@
+// ///////////////Question1///////////
+
+// alert("")
+// // alert( "Welcom to my page");
+// console.log("my name");
+// document.write("My Name is HUraira");
+
+
+// var line1 = "Hello my name is Huraira";
+// var line2 = "I am frontend developer";
+// var message = line1 + "\n" +line2 ;
+// function lineBreak() {    
+// }
+// alert(message);
+
+
+// var line1 = "Happy Coading!" ;
+// var line2 = "Prevent this page from creating additional diolougs." ;
+// var message = line1+ "\n" + line2;
+// function lineBreak() {}
+// alert(message)    
+
+
+// var message = "Hello.. i can run just through my web browser console";
+// console.log(message);
+//   /////////////end////////////
+
+// ///////////////////////////Question2////////////////
+
+// var username = ("") ;
+// var username = ("") ;
+// var message =("Hello world");
+
+// alert(message);
+
+// var name =("Huraira");
+// var age = +("20");
+// var work =("Fron-end developer");
+
+// alert (name);
+// alert (age);
+// alert (work);
+
+
+// var line1 = "Pizza" ;
+// var line2 = "Pizz" ;
+// var line3 = "Piz" ;
+// var line4 = "Pi" ;
+// var line5 = "P" ;
+// var message =line1+ "\n" +line2+ "\n"+line3+"\n"+line4+"\n"+line5;
+// function lineBreak(){    
+// } 
+// alert(message);
+
+// var message ="My email address:Muhammadhuraira@gmail.com";
+// alert(message);
+
+// var line1 = "I am trying to learn from the book A smart";
+// var line2 = "way to learn jawascript";
+// var message =line1+"\n"+line2 ;
+// function lineBreak(){};
+// alert (message);
+
+// var message ="Yah! <b>I</b> can write <b>Html</b> content thorugh javascript!";
+// document.write(message);
+
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+// ////////////////////////End/////////////////////////
+
+// ///////////////////////Question3///////////////////////
+
+// alert ("I am 20 years old");
+
+// // alert ("You have visited this site 14 times");
+
+// var message="My birth year is 2002 <br> Deta type of my decleard veriables is number";
+// // document.write (message);
+
+// var message="<b>Jhon Doe</b> orderd <b>5 T.shirt</b>(s)on XYZ clothing store";
+// document.write(message);
+
+// ///////////////////////End/////////////////////////
+
+// //////////////Question4///////////////////
+
+// var username ="Huraira1";
+// var username17 ="Huraira2";
+// var userName ="Huraira3";
+// var user_name ="Huraira4";
+// var $username ="Huraira5";
+
+// document.write(username);
+// document.write(userName);
+// document.write(username17);
+// document.write(user_name);
+// document.write($username);
+
+// /////////////////////End//////////////////////
+
+// ///////////////////Question5////////////////////////
+
+// var num1 = 3;
+// var num2 = 5;
+// document.write ("sum of 3 and 5 is");
+// document.write(num1 + num2);
+
+
+// var num1 = 9;
+// var num2 = 5;
+// document.write ("sum of 9 and 5 is");
+// document.write(num1 - num2);
+
+// var num1 = 3;
+// var num2 = 5;
+// document.write ("sum of 3 and 5 is");
+// document.write(num1 * num2);
+
+// var num1 = 2;
+// var num2 = 20 ;
+// document.write ("sum of 2 and 20 is");
+// document.write(num1 % num2);
+
+// var value = 600;
+// document.write (value);
+// document.write ("<br\>");
+// document.write (typeof value);
+
+// var 22value = ;
+// document.write ("it.s not work");
+
+// document.write (5);
+  
+// document.write("Value after the decleration is undefind");
+// document.write("<br/>");
+// document.write("<br/>");
+// document.write("Initial Value is :5");
+// document.write("<br/>");
+// document.write("<br/>");
+// document.write("Value after increment:");
+// var a =5;
+// var value =++a;
+// document.write(a);
+// document.write("<br/>");
+// document.write("<br/>");
+// document.write("Value after addition is:");
+// var a =6;
+// var value =7;
+// document.write(a + value);
+// document.write("<br/>");
+// document.write("<br/>");
+// document.write("The value after decriment is:");
+// var a = 13;
+// var value1 =--a;
+// document.write(a);
+// document.write("<br/>");
+// document.write("<br/>");
+// document.write("The remender is:0");
+
+// document.write("Totle cost to buy 5 tickets to a movie is:");
+// document.write(6000 * 5);
+// document.write("<b>PKR<b/>");
+
+
+// document.write("Table of 4");
+// document.write("<br/>");
+// document.write("4 x 1 =");
+// document.write( 4*1 );
+// document.write("<br/>");
+// document.write("4 x 2 =");
+// document.write( 4*2 );
+// document.write("<br/>");
+// document.write("4 x 3 =");
+// document.write( 4*3 );
+// document.write("<br/>");
+// document.write("4 x 4 =");
+// document.write( 4*4 );
+// document.write("<br/>");
+// document.write("4 x 5 =");
+// document.write( 4*5 );
+// document.write("<br/>");
+// document.write("4 x 6 =");
+// document.write( 4*6 );
+// document.write("<br/>");
+// document.write("4 x 7 =");
+// document.write( 4*7 );
+// document.write("<br/>");
+// document.write("4 x 8 =");
+// document.write( 4*8 );
+// document.write("<br/>");
+// document.write("4 x 9 =");
+// document.write( 4*9 );
+// document.write("<br/>");
+// document.write("4 x 10 =");
+// document.write( 4*10 );
+
+// document.write("<h1>Shopping Cart</h1>");
+// document.write("<br/>");document.write("<br/>");document.write("<br/>");document.write("<br/>");
+// document.write("Price of item 1 is 650");
+// document.write("<br/>");
+// document.write("Quentity of item 1 is 3");
+// document.write("<br/>");
+// document.write("Price of item 1 is 650");
+// document.write("<br/>");
+// document.write("Price of item 1 is 100");
+// document.write("<br/>");
+// document.write("Quentity of item 2 is 7");
+// document.write("<br/>");
+// document.write("Shipping Charges is 100");
+// document.write("<br/>");
+// document.write("<br/>");
+// document.write("<br/>");
+// document.write("<br/>");
+// document.write("Totle cost of your order is:");
+// var item1 = 650;
+// var quantity1 = 3;
+// var item2 = 100;
+// var quantity2 = 7;
+// var extracharges = 100;
+// document.write (item1 * quantity1 + item2 * quantity2 + extracharges);
+
+// document.write("<h1>Marks Sheet</h1>");
+// document.write("<br/>");document.write("<br/>");document.write("<br/>");document.write("<br/>");
+// document.write("Totle MArkes 980");document.write("<br/>");
+// document.write("MArkes Obtained 840");document.write("<br/>");
+// document.write("Percentage");
+// var Totle = 980;
+// var Obtained =840;
+// var Percentage =(Totle * 100 ) / Obtained;
+// document.write(Totle *100 / Obtained);
+
+// document.write("<h1>Currency in PKR</h1>");document.write("<br/>");
+// document.write("Totle Currency in PKR:");
+// var amount1 = 104.80;
+// var amount2 = 28;
+// document.write( amount1 * 10 + amount2 * 25);
+
+
+// document.write("<h1>AGe Calculator</h1>");document.write("<br/>");document.write("<br/>");document.write("<br/>");
+// document.write("Curretn Year:2023");document.write("<br/>");
+// document.write("Birth Year:2002");document.write("<br/>");
+// document.write("Your age is:");
+// var Curretn = 2023;
+// var birth = 2002;
+// document.write(2023 - 2002);
+
+
+// document.write("<h1>The lifetime supply Calculator</h1>");document.write("<br/>");document.write("<br/>");document.write("<br/>");
+// document.write ("Favourite Snack:chololate chip");document.write("<br/>");
+// document.write("Current age: 15");document.write("<br/>");
+// document.write ("Estimated Maximum Age:65");document.write("<br/>");
+// document.write ("Amount of snack per day:3");document.write("<br/>");
+// document.write ("You will need 150 chocolates chip to last you until the ripe old age of");document.write("<br/>");
+
+
+// document.write("Result");document.write("<br/>");
+// document.write("The value of a is : 10");document.write("<br/>");document.write("<br/>");
+// document.write("<b>............................................</b>");document.write("<br/>");document.write("<br/>");
+// document.write ("The value of  ++a  is : ");
+
+// var a= 10;
+// var value = ++a ;
+// console.log(value);
+// document.write(a); document.write("<br/>");
+// document.write ("Now the value of  ++a  is : 11");document.write("<br/>");document.write("<br/>");
+
+// document.write("THe value of a++ is : 11");document.write("<br/>");
+// document.write("NOw the value of a is :");
+// var a = 11;
+// var value = a++;
+// document.write(a);document.write("<br/>");document.write("<br/>");document.write("<br/>");
+
+// document.write("THe value of --a is : 11");document.write("<br/>");
+// document.write("NOw the value of a is :");
+// var a = 12;
+// var value = --a;
+// document.write(a);document.write("<br>");document.write("<br/>");document.write("<br/>");
+
+// document.write("THe value of a-- is : 11");document.write("<br/>");
+// document.write("NOw the value of a is :");
+// var a = 11;
+// var value = a--;
+// document.write(a);
+
+
+// var a=2 , b=1;
+// var result = --a - --b + ++b + b--;
+// document.write("a is : ");
+// document.write(a + result);document.write("<br/>");
+// document.write("b is : ");
+// document.write(b + result);document.write("<br/>");
+// document.write("Result is : ");
+// document.write(a,b + result);
+
+
+
+// var city = prompt ("Enter Your city");
+// if(city == "Karachi"){
+// console.log("Allow");
+// document.write("Welcome to the city of light!")
+// }else{
+//   console.log("not allow");
+//   document.write("Sorry")
+// }
+
+// var city = prompt("Enter your city")
+
+// if (city == "karachi") {
+//   console.log("ALLOW");
+//   document.write("Welcome to the city of light!");
+// } else {
+//   console.log("NOT ALLOW")
+//   document.write("Sorry");
+//  }
+
+//  document.write("<br>");
+// var gender = prompt ("Enter your gender");
+// if(gender == "male"){
+// console.log("Allow");
+// document.write("Good Morning Sir!")
+// }else{(gender=="female") 
+// console.log("Ok");
+// document.write("Good Morning Ma,am")
+// }
+
+// document.write("<table border='1px'> <th><tr>Signal</tr></th><th><tr>Message</tr></th> <tr><td>Red</td></tr> <tr><td>Yellow</td></tr> <tr><td>Green</td></tr> <br /> <tr><td>Must be ready</td></tr> <tr><td>Ready to go</td></tr> <tr><td>Move Now</td></tr></table>")
+
+// var fule = prompt ("Enter Remening fule in car");
+// if(fule <= "0.25 litters"){
+//   console.log("Not Ok");
+//   alert("Please refile the fule in your car")
+//   document.write("Please refile the fule in your car")
+// }else{
+//   console.log("Ok");
+//   alert("This time you don,t need fule");
+//   document.write("This time you don,t need fule")
+// }
+
+// var a = 4;
+// if(++a ===5 ){
+//   console.log("Allow");
+//   alert("Given condation for a is true!")
+//   document.write("Given condation for a is true!")
+// }else{
+//   console.log("NOt Allow");
+//   alert("Given condation for a is not true")
+//   document.write("Given condation for a is not true!")
+
+// }
+
+
+// var b = 82;
+// if(++b ===83 ){
+//   console.log("Allow");
+//   alert("Given condation for b is true!")
+//   document.write("Given condation for b is true!")
+// }else{
+//   console.log("NOt Allow");
+//   alert("Given condation for b is not true")
+//   document.write("Given condation for b is not true!")
+// }
+
+
+// var c = 12;
+// if(c++ === 13 ){
+//   console.log("Allow");
+//   alert("condation 1 is true!")
+//   document.write("condation 1 is true!")
+// }else{
+//   console.log("NOt Allow");
+//   alert("condation 1 is not true")
+//   document.write("condation 1 is not true!")
+// }
+// document.write("<br>");
+// if(c === 13 ){
+//   console.log("Allow");
+//   alert("condation 2 is true!")
+//   document.write("condation 2 is true!")
+// }else{
+//   console.log("NOt Allow");
+//   alert("condation 2 is not true")
+//   document.write("condation 2 is not true!")
+// }
+
+// if(++c <14 ){
+//   console.log("Allow");
+//   alert("condation 3 is true!")
+//   document.write("condation 3 is true!")
+// }else{
+//   console.log("NOt Allow");
+//   alert("condation 3 is not true")
+//   document.write("condation 3 is not true!")
+// }
+
+// if(c === 14 ){
+//   console.log("Allow");
+//   alert("condation 3 is true!")
+//   document.write("condation 3 is true!")
+// }else{
+//   console.log("NOt Allow");
+//   alert("condation 3 is not true")
+//   document.write("condation 3 is not true!")
+// }
+
+// var metrialcost = 2000;
+// var labourcost = 2000;
+// var totlecost = labourcost + metrialcost;
+// if(totlecost === labourcost + metrialcost){
+// alert("Cost is Equal")
+// }else{
+//   alert("COst is not equal")
+// }
+
+// if("true"){
+//   alert("True")
+// }else{
+//   alert("false")
+// }
+// if("false"){
+//   alert("false")
+// }else{
+//   alert("true")
+// }
+
+// if("Car"<"Cat"){
+// alert("Car is smaller then Cat")
+// }
+// var a = 4
+// var pro = prompt("guess the number")
+// if(a == pro){
+//   alert("Bingo'Correct answer")
+// }else{
+//   alert("wrong answer")
+// // }
+// var a = +prompt()
+// if(a / 3 == 0){
+// alert("no is divisibel by 3")
+// }else{
+//   alert("not divisibel")
+// }
+
+// var a = +prompt();
+// if(a % 2 == 0){
+// alert("Even Number")
+// }else{
+//   alert("Odd Number")
+// }
+
+// var percentage = +prompt("Enter Your percantage")
+// if( percentage >= 80  && percentage <= 100 ){
+//   console.log("A+");
+// document.write("A+");                                            
+// document.write("Excellent")
+// }else{}
+// if( percentage >= 60  && percentage <= 80 ){
+//   console.log("A");
+// document.write("A");                                            
+// document.write("Good")
+// }else{}
+// if( percentage >= 40  && percentage <= 600 ){
+//   console.log("B");
+// document.write("B");                                            
+// document.write("Better")
+// }else{}
+
+// document.write("<h1>Mark sheet</h1>");document.write("<br>");document.write("<br>");document.write("<br>");document.write("<br>");
+
+
+// var Totle = 300;
+// var Obtaind = 219;
+// var percantage =(Obtaind / Totle * 100);
+// document.write("Totle Marks: 300");document.write("<br>");
+// document.write("Marks Obtaind :219");document.write("<br>");
+// document.write("Percentage :");
+// document.write(percantage);
+// document.write("%");document.write("<br>");
+// document.write("Grade : B");document.write("<br>");
+// document.write("Remarks : You Need to improve")
+
+
+// var num1 = +prompt("Enter num1");
+// var opt = prompt("Enter opt");
+// var num2 = +prompt("Enter num2");
+// if(opt ==="+"){
+//   console.log(num1 + num2)
+//   document.write(num1 + num2)
+// } else if(opt==="-"){
+//   console.log(num1 - num2)
+//   document.write(num1 - num2)
+// }else if(opt==="*"){
+//     console.log(num1 * num2)
+//     document.write(num1 * num2)
+//   }else{
+//     console,log("Inveled opt")
+//     document.write("Inveled opt")
+//   }
+
+// var A = prompt(65);
+// var Z = prompt(90);
+// var a = prompt(97);
+// var z = prompt(122); 
+// if( "A" = 65){
+//   console.log(A);
+//   document.write(A)
+// }else if("Z" = 65){
+//   console.log(Z);
+//   document.write(Z)
+// }else if("a" = 65){
+//   console.log(a);
+//   document.write(a)
+// }else{
+//   console.log(z)
+//   document.write(z)
+// }
+
+// var A = prompt("");
+// var Z = 90;
+// var a = 97;
+// var z = 122 ;
+//  if(A = 65){
+// alert(65)
+// }else if( Z = 90){
+//  alert("90")
+// }else if(a==97){
+// alert("97")
+// }else if(z ==122){
+//   alert("97")
+//   }else{
+//     console.log("Not allow")
+//   }
+
+
+// var username17 = +prompt("ENter number or Letter");
+// var
+// // if(username17 === "")
+// // var value = "20"
+// console.log(typeof username17)
+//  if (typusername17 === "",20) {
+//  console.log("OK")
+//  } else {
+//   console.log("false")
+//  }
+
+
+// var userAge = +prompt("Enter your age");
+// var userGender = prompt("Enter your gender");
+
+
+//  if (userAge == 20 && userGender == "female") {
+//  console.log("USER ALLOW")
+//  } else {
+//  console.log("USER NOT ALLOW")
+// }
+// var userAge = +prompt("Enter your age");
+// var userGender = prompt("Enter your gender");
+//  if (userAge == 20 && userGend   er == "female","male") {
+//  console.log("USER ALLOW");
+//  document.write("USER ALLOW")
+//  } else {
+//  console.log("USER NOT ALLOW");
+//  document.write("USER NOT ALLOW")
+// //  }
+// var age = +prompt("Enter Your age")
+// if(age == 20){
+//     console.log("The age is equal");
+//     document.write("The age is equal");
+//     alert("The age is equal")
+// }else if (age < 20){
+//     console.log("The age is less");
+//     document.write("The age is less")
+//     alert("The age is less")
+// }else{
+//     console.log("The age is above");
+//     document.write("The age is above")
+//     alert("The age is above")
+// }
+
+// var value= +prompt("ENter the Number")
+// if(value >=1 ){
+// console.log("The Number is positive");
+// document.write("The Number is positive");
+// alert("The Number is positive")
+// }else if(value <0){
+//     console.log("The Number is Nagetive");
+//     document.write("The Number is Nagetive");
+//     alert("The Number is Nagetive")
+// }else{
+// console.log("The Number is Zero");
+// document.write("The Number is Zer");
+// alert("The Number is Zero")
+// }
+
+// var value= prompt("Enter letter");
+// if(value === "a,i,o,u"){
+//     console.log("True");
+//     document.write("True");
+//     alert("True")
+// }else{
+//     console.log("False");
+//     document.write("False");
+//     alert("False")
+// }
+
+
+// var password ="wordpass";
+// var response;
+// var enteryCount=0;
+// var enteryLimit=6;
+// var error= "false";
+// while(response != password && !error){
+// if(enteryCount < enteryLimit){
+//     response = window.prompt("Enter Password") 
+// enteryCount++
+// }else{
+// error= true;
+// }
+// }if(error){
+//     alert("to many entries");
+// }else{
+//     alert("YOu Got it!")
+// }
+
+
+// var greeting;
+// var hour=13;
+// if(hour < 18){
+//     document.write("Good day");
+//     alert("Good Day")
+// }else{
+    
+//     document.write("Good Evening");
+//     alert("Good Evening")
+// }
+
+
+
